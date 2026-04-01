01c12ea754cae463a72d7a713ecaf6882ef4bf0f
anweshadas09 <anweshadas34138@gmail.com>
2026-04-02 00:49:22 +0530

    Add complete Django project with fixed DateField and all components

---
+++ newProject/newApp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Product(models.Model):
+    Product_Type = (
+        ('Electronics', 'Electronics'),
+        ('Clothing', 'Clothing'),
+        ('Books', 'Books'),
+    )
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    date = models.DateField(default=timezone.localdate)
+    product_type = models.CharField(max_length=100, choices=Product_Type)
+    description = models.TextField(default="")
+    
+    def __str__(self):
+        return self.name
+
+# Create your models here.
